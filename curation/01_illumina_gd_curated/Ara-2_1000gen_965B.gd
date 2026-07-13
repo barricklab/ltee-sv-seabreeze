b6dc1d78dcd17f0ff71bf5bd467eff8b1c91cdd6
Ira Zibbu <ira.zibbu@gmail.com>
2026-07-13 15:02:04 -0400

    Init commmit of gd files

---
+++ curation/01_illumina_gd_curated/Ara-2_1000gen_965B.gd
@@ -0,0 +1,14 @@
+#=GENOME_DIFF	1.0
+#=TITLE	Ara-2_1000gen_965B
+#=TIME	1000
+#=POPULATION	Ara-2
+#=TREATMENT	LTEE
+#=CLONE	B
+#=MUTATOR_STATUS	non-mutator
+#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/005/SRR2584465/SRR2584465_1.fastq.gz
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/005/SRR2584465/SRR2584465_2.fastq.gz
+SNP	1	.	REL606	649522	T
+SNP	2	.	REL606	1733754	T
+INS	3	.	REL606	2103918	CCAG	repeat_length=4	repeat_new_copies=8	repeat_ref_copies=7	repeat_seq=CCAG
+SNP	4	.	REL606	3762120	A
---
+++ curation/01_illumina_gd_curated/Ara-2_1500gen_1069A.gd
@@ -0,0 +1,15 @@
+#=GENOME_DIFF	1.0
+#=TITLE	Ara-2_1500gen_1069A
+#=TIME	1500
+#=POPULATION	Ara-2
+#=TREATMENT	LTEE
+#=CLONE	A
+#=MUTATOR_STATUS	non-mutator
+#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR259/003/SRR2591033/SRR2591033_1.fastq.gz
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR259/003/SRR2591033/SRR2591033_2.fastq.gz
+SNP	1	.	REL606	649522	T
+SNP	2	.	REL606	1733754	T
+INS	3	.	REL606	2103918	CCAG	repeat_length=4	repeat_new_copies=8	repeat_ref_copies=7	repeat_seq=CCAG
+SNP	4	.	REL606	3762120	A
+DEL	5	.	REL606	3894997	5681	mediated=IS150
